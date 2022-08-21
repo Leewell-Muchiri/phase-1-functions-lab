e3e42eb28e4f7a6374ad822e4350a43c7510a76f
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-21 16:06:26 +0300

    Complted lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+let blocks = distanceFromHqInBlocks
+let feet = distanceFromHqInFeet
+let blocks1 = destination
+
+
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42){
+    return blocks - 42;
+    }
+
+    else if (blocks <= 42){
+    return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks){
+    return (distanceFromHqInBlocks(blocks)*264);
+}
+
+function distanceTravelledInFeet(blocks,blocks1){
+    const distanceTravelledInFeet = (distanceFromHqInBlocks(blocks - blocks1)*264);
+    if (distanceTravelledInFeet >= o){
+        return distanceTravelledInFeet;
+    } else {
+        return (distanceTravelledInFeet * -1);
+    }
+}
+
+function calculatesFarePrice(blocks,blocks1){
+    if (distanceTravelledInFeet <= 400){
+       return 0;
+    }
+    else if ((distanceTravelledInFeet > 400) && (distanceTravelledInFeet <=2000)){
+       return ((distanceTravelledInFeet - 400)*0.02)
+    }
+    else if ((distanceTravelledInFeet > 2000) && (distanceTravelledInFeet <=2500)){
+       return 25;
+    }
+    else {
+       return 'cannot travel that far';
+    }
+}
